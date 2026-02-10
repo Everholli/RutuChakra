784ae3f441bb417901eb4dfddac329f9cf06be32
Everholli <shrutimishra2522@gmail.com>
2026-02-10 15:35:26 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/components/ui/calendar.tsx
+++ frontend/components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
+import "react-day-picker/style.css";
 
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
@@ -54,8 +55,7 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        CaptionLabel: (props) => <div {...props} />,
       }}
       {...props}
     />
